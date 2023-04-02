a26518d9b17727ba0f742e6fb67f9e06106adf5c
mutai-gilbert <mutaigilbert85@gmail.com>
2023-04-02 20:33:36 +0300

    Added new files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './components/Table';
+
+const App =() => {
+  return (
+    <div className="table_Container">
+      <h1> Sortable Table With React</h1>
+      <Table />
+
+    </div>
+  )
+}
+
+export default App;
